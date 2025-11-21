617a2c18420bab09cac5a86708f0537554559b6a
Dias <dias@MacBook-Air-Dias-3.local>
2025-11-21 11:03:00 +0500

    first commit

---
+++ lab8/a.cpp
@@ -0,0 +1,72 @@
+#include <bits/stdc++.h>
+#define dalshe ios_base::sync_with_stdio(false);
+#define menshe cin.tie(NULL);cout.tie(NULL);
+#define all(x) x.begin(), x.end()
+#define pii pair<int,int>
+#define F first
+#define ll long long
+#define ld long double
+#define S second
+#define mp make_pair
+#define pb push_back
+#define in insert
+#define endl '\n'
+
+using namespace std;
+
+const ll N = 2e5 + 17;
+const ll inf = 1e9 + 7;
+const ll mod = 1e9 + 7;
+
+vector<string> v;
+unordered_set<string> st;
+
+string hashing(const string &s) {
+    ll h = 0, p = 1;
+    for (char c : s) {
+        h = (h + ((c - 47) * p) % mod) % mod;
+        p = (p * 11) % mod;
+    }
+    return to_string(h);
+}
+
+void solve(){
+    dalshe menshe
+
+    int n; string s;
+
+    cin >> n;
+    for(int i = 1; i <= n * 2; i++){
+        cin >> s;
+        v.push_back(s);
+        st.insert(s);
+    }
+
+    int cnt = 0;
+
+    for(int i = 0; i < v.size(); i++){
+        if(cnt == n) break;
+        string hs = hashing(v[i]);
+        if(st.find(hs) != st.end()){
+            cout << "Hash of string \"" << v[i] << "\"" << " is " << hs << endl;
+            cnt++;
+        }
+    }
+}
+
+signed main() {
+
+    dalshe menshe
+
+    int tt = 1;
+
+    
+
+    while(tt--){
+
+        solve();
+
+    }
+
+    return 0;
+}
---
+++ lab8/f.cpp
@@ -0,0 +1,81 @@
+#include <bits/stdc++.h>
+#define dalshe ios_base::sync_with_stdio(false);
+#define menshe cin.tie(NULL);cout.tie(NULL);
+#define all(x) x.begin(), x.end()
+#define pii pair<int,int>
+#define F first
+#define ll long long
+#define ld long double
+#define S second
+#define mp make_pair
+#define pb push_back
+#define in insert
+#define endl '\n'
+
+using namespace std;
+
+const ll N = 2e5 + 17;
+const ll inf = 1e9 + 7;
+const ll mod1 = 1e9 + 7;
+const ll mod2 = 1e9 + 9;
+const ll pw1 = 31;
+const ll pw2 = 37;
+
+vector<ll> h1(2002, 0), h2(2002, 0);
+vector<ll> p1(2002, 1), p2(2002, 1);
+
+pair<ll, ll> get_hash(int l, int r) {
+    pair<ll, ll> res = {(h1[r] - h1[l] * p1[r - l]) % mod1, (h2[r] - h2[l] * p2[r - l]) % mod2};
+    if (res.first < 0) res.first += mod1;
+    if (res.second < 0) res.second += mod2;
+    return res;
+}
+
+void solve(){
+    dalshe menshe
+
+    string s;
+    unordered_set<uint64_t> st;
+
+    cin >> s;
+
+    int n = s.size();
+
+    for (int i = 0; i < n; i++) {
+        h1[i + 1] = (h1[i] * pw1 + (s[i] - 'a' + 1)) % mod1;
+        h2[i + 1] = (h2[i] * pw2 + (s[i] - 'a' + 1)) % mod2;
+    }
+
+    for(int i = 1; i <= s.size(); i++){
+        for(int j = 0; j + i <= s.size(); j++){
+            pair<ll, ll> p = get_hash(j, j + i);
+            uint64_t combine = ((uint64_t)p.F << 32) | ((uint64_t)p.S);
+            st.insert(combine);
+        }
+    }
+    cout << st.size();
+
+}
+
+
+signed main() {
+
+    dalshe menshe
+
+    int tt = 1;
+
+    //cin >> tt;
+
+    for (int i = 1; i < 2002; i++) {
+        p1[i] = (p1[i-1] * pw1) % mod1;
+        p2[i] = (p2[i-1] * pw2) % mod2;
+    }
+
+    while(tt--){
+
+        solve();
+
+    }
+
+    return 0;
+}
